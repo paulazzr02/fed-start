4dbfe5d5d31145252877a8464aec7f2132d5b570
tykim <tykim1499@gmail.com>
2020-04-06 12:59:19 +0900

    #2 basic template

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,90 @@
 // FILE: main.js
 
-// From node_modules folder
-import jQuery from 'jquery';
-window.$ = window.jQuery = jQuery;
-import 'retinajs/dist/retina';
-import SimpleScrollbar from 'simple-scrollbar';
-// import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap';
+// from node_modules folder
+import 'matchmedia-polyfill'
+import 'matchmedia-polyfill/matchMedia.addListener'
+import jQuery from 'jquery'
+window.$ = window.jQuery = jQuery
+import 'retinajs/dist/retina'
+import SimpleScrollbar from 'simple-scrollbar'
+import 'bootstrap'
+
+import Dom from './modules/dom'
+
+/* foreach polyfill */
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+/**
+ * Element.matches() polyfill (simple version)
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ */
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+const log = console.log
+const body = document.body
+
+const mediaQueryList = matchMedia('screen and (max-width: 767px) and (orientation: portrait)')
+const detectMediaSize = () => {
+
+  if ( mediaQueryList.matches ) {
+    // log('Mobile');
+    Dom.removeClass(body, 'contextual-sidebar')
+    Dom.addClass(body, 'contextual-sidebar-mobile')
+  } else {
+    // log('PC');
+    Dom.removeClass(body, 'contextual-sidebar-mobile')
+    Dom.addClass(body, 'contextual-sidebar')
+  }
+
+}
+
+const handleSidebarToggle = () => Dom.toggleClass(body, 'contextual-sidebar-active')
+
+// App
+const app = () => {
+
+  // Toggle sidebar
+  detectMediaSize()
+
+  // Scroll menu using simple-scrollbar
+  const scrollDivs = document.querySelectorAll('.js-simple-scrollbar')
+  scrollDivs.forEach((el) => SimpleScrollbar.initEl(el))
+
+  // Customize Bootstrap Accordion
+  $('.collapse').on('show.bs.collapse', function() {
+    $(this).parent('li').addClass('active')
+  });
+  $('.collapse').on('hide.bs.collapse', function() {
+    $(this).parent('li').removeClass('active')
+  });
+  $('.js-collapse-nav').click(function() {
+    $(this).next().collapse('toggle')
+  });
+  $('#accordionMenu').find('.collapse:first')
+    .addClass('show')
+    .parent('li')
+    .addClass('active')
+
+};
+
+document.addEventListener('DOMContentLoaded', app, false)
+document.querySelector('.body-overlay').addEventListener('click', handleSidebarToggle)
+document.querySelector('.js-toggle-sidebar').addEventListener('click', handleSidebarToggle)
 
 // Auto resizing textarea height
-const dataAutoResizing = (el) => {
-  element.style.boxSizing = 'border-box';
-  let offset = element.offsetHeight - element.clientHeight;
-  document.addEventListener('input', (e) => {
-    e.target.style.height = 'auto';
-    e.target.style.height = e.target.scrollHeight + offset + 'px';
-  });
-  element.removeAttribute('data-autoresize');
-}
+// const dataAutoResizing = (el) => {
+//   element.style.boxSizing = 'border-box';
+//   let offset = element.offsetHeight - element.clientHeight;
+//   document.addEventListener('input', (e) => {
+//     e.target.style.height = 'auto';
+//     e.target.style.height = e.target.scrollHeight + offset + 'px';
+//   });
+//   element.removeAttribute('data-autoresize');
+// }
 
 // function scrollToTop() {
 //   (function smoothscroll() {
@@ -54,36 +121,36 @@ const dataAutoResizing = (el) => {
 //     }
 // }
 
-const app = () => {
-    const scrollDivs = document.querySelectorAll('.js-simple-scrollbar');
-    scrollDivs.forEach((el) => {
-      SimpleScrollbar.initEl(el);
-    });
-
-    const autoResizeableTextareas = document.querySelectorAll('[data-autoresize]');
-    autoResizeableTextareas.forEach((el) => {
-      dataAutoResizing(el)
-    });
-
-    // onScroll();
-};
-document.addEventListener('DOMContentLoaded', app);
-
-let rtime, timeout = false, delta = 200;
-const doneResizing = () => {
-  if (new Date() - rtime < delta) {
-    setTimeout(doneResizing, delta);
-  } else {
-    timeout = false;
-    app;
-    console.log('Done resizing');
-  }
-}
-// Window resizing...
-window.addEventListener('resize', () => {
-  rtime = new Date();
-  if (timeout === false) {
-    timeout = true;
-    setTimeout(doneResizing, delta);
-  }
-});
+// const app = () => {
+//     const scrollDivs = document.querySelectorAll('.js-simple-scrollbar');
+//     scrollDivs.forEach((el) => {
+//       SimpleScrollbar.initEl(el);
+//     });
+//
+//     const autoResizeableTextareas = document.querySelectorAll('[data-autoresize]');
+//     autoResizeableTextareas.forEach((el) => {
+//       dataAutoResizing(el)
+//     });
+//
+//     // onScroll();
+// };
+// document.addEventListener('DOMContentLoaded', app);
+//
+// let rtime, timeout = false, delta = 200;
+// const doneResizing = () => {
+//   if (new Date() - rtime < delta) {
+//     setTimeout(doneResizing, delta);
+//   } else {
+//     timeout = false;
+//     app;
+//     console.log('Done resizing');
+//   }
+// }
+// // Window resizing...
+// window.addEventListener('resize', () => {
+//   rtime = new Date();
+//   if (timeout === false) {
+//     timeout = true;
+//     setTimeout(doneResizing, delta);
+//   }
+// });
